04545a1ccf7092cb2e45e1a5e18db4c80a4c986d
Mattia Righetti <matt95.righetti@gmail.com>
2020-07-09 14:38:44 +0200

    Code refactoring

--- main.py
+++ main.py
@@ -14,44 +14,44 @@ def generate_random_directed_weighted_graph(num_nodes=10):
     return DAG
 
 
-def _gen_matrix_w(graph):
-    """
-    Generates a W matrix.
-    The quantity W(u,v) is the minimum number of registers on any path from vertex u to vertex v.
-    :param graph: directional acyclic graph
-    :return: W matrix
-    """
-    n = max(max(graph.edges()))
-    W = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-
-    for (u, v) in graph.edges():
-        try:
-            shortest_path = nx.dijkstra_path_length(graph, u, v)
-            W[u - 1][v - 1] = shortest_path
-        except nx.NetworkXNoPath:
-            W[u - 1][v - 1] = -2
-
-    return W
-
-
-def _gen_matrix_d(W_matrix, register_weight):
-    D = [[i * register_weight for i in W_matrix[j]] for j in range(len(W_matrix))]
-    return D
-
-
-def gen_wd_matrices(graph, register_weight):
-    W_matrix = _gen_matrix_w(graph)
-    D_matrix = _gen_matrix_d(W_matrix, register_weight)
-    return W_matrix, D_matrix
+def read_from_file(from_file):
+    raise Exception("Not implemented")
 
 
 class DAGraph(object):
-    def __init__(self, num_nodes, register_weight):
+    def __init__(self, num_nodes, register_weight, from_file=None):
         self.num_nodes = num_nodes
         self.register_weight = register_weight
 
-        self.dag = generate_random_directed_weighted_graph(num_nodes)
-        self.w, self.d = gen_wd_matrices(self.dag, self.register_weight)
+        if from_file is None:
+            self.dag = generate_random_directed_weighted_graph(num_nodes)
+        else:
+            self.dag = read_from_file(from_file)
+
+        self.w = self._gen_matrix_w()
+        self.d = self._gen_matrix_d()
+
+    def _gen_matrix_w(self):
+        """
+        Generates a W matrix.
+        The quantity W(u,v) is the minimum number of registers on any path from vertex u to vertex v.
+        :return: W matrix
+        """
+        n = max(max(self.dag.edges()))
+        W = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+        for (u, v) in self.dag.edges():
+            try:
+                shortest_path = nx.dijkstra_path_length(self.dag, u, v)
+                W[u - 1][v - 1] = shortest_path
+            except nx.NetworkXNoPath:
+                W[u - 1][v - 1] = -2
+
+        return W
+
+    def _gen_matrix_d(self):
+        D = [[i * self.register_weight for i in self.w[j]] for j in range(len(self.w))]
+        return D
 
 
 if __name__ == '__main__':
