90be0ee56c8c550857a36d5b8153423a7da1da9e
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-09 23:12:18 +0200

    Init OPT1

--- utils/algorithms/opt1.py
+++ utils/algorithms/opt1.py
@@ -1,11 +1,12 @@
 import networkx as nx
 import numpy as np
 from utils.algorithms.wd import wd
+from utils.algorithms.bellman_ford import binary_search
 
 __all__ = ['opt1']
 
 
-def opt1(graph: nx.DiGraph) -> int:
+def opt1(graph: nx.DiGraph):
     # 1. Calculate WD
     W, D = wd(graph)
 
@@ -14,12 +15,7 @@ def opt1(graph: nx.DiGraph) -> int:
 
     # 3. Binary search minimum clock period in D(u, v) values using Bellman-Ford algorithm
     #     to check if Theorem 7 can be satisfied
-    binary_search_minimum_clock_period(sorted_D)
+    min_clock_period, retiming = binary_search(graph, sorted_D, W)
 
     # 4. For the minimum found before, use values of r(u) found by Bellman-Ford as the optimal retiming
-
-    return 0
-
-
-def binary_search_minimum_clock_period(sorted_D):
-    raise NotImplemented("binary_search_minimum_clock_period has not yet been implemented")
+    return min_clock_period, retiming
