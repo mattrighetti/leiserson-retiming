dfcb73d213f4c226c33358897b691a0acf304787
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-09 14:35:05 +0200

    Removed useless files

--- utils/test_functions.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-
-
-def is_retiming_legal(graph: nx.DiGraph) -> bool:
-    for e in graph.edges:
-        if graph.edges[e]["weight"] < 0:
-            return False
-    return True
