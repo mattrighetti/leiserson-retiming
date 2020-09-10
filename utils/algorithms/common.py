34a8f18570d441fd50acb3c010f30a8d9b4031d8
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-10 16:02:04 +0200

    Implemented function to apply retiming on graph

--- utils/algorithms/common.py
+++ utils/algorithms/common.py
@@ -1,6 +1,17 @@
 import networkx as nx
 
-__all__ = ['get_subgraph_with_weight']
+__all__ = ['apply_retiming',
+           'get_subgraph_with_weight']
+
+
+def apply_retiming(graph: nx.DiGraph, r) -> nx.DiGraph:
+    retimed_graph = nx.DiGraph()
+
+    retimed_graph.add_weighted_edges_from(
+        [(u, v, graph.edges[u, v]['weight'] + r.get(v, 0) - r.get(u, 0)) for (u, v) in graph.edges]
+    )
+
+    return retimed_graph
 
 
 def get_subgraph_with_weight(graph: nx.DiGraph, weight, copy=True) -> nx.DiGraph:
