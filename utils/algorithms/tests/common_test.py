4d7996c234f88508ad9c851c3cd4cc9ad4b34586
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-10 17:40:04 +0200

    Added apply_retiming_test

---
+++ utils/algorithms/tests/common_test.py
@@ -0,0 +1,38 @@
+import networkx as nx
+import numpy as np
+from utils.algorithms.common import apply_retiming
+
+
+def test_apply_retiming():
+    graph = nx.DiGraph()
+    graph.add_nodes_from([
+        (0, {'delay': 0}),
+        (1, {'delay': 3}),
+        (2, {'delay': 3}),
+        (3, {'delay': 7})
+    ])
+    graph.add_weighted_edges_from([(0, 1, 2.0),
+                                   (1, 2, 0.0),
+                                   (1, 3, 0.0),
+                                   (2, 3, 0.0),
+                                   (3, 0, 0.0)])
+
+    retiming = {
+        0: 0.0,
+        1: -1.0,
+        2: -1.0,
+        3: 0.0
+    }
+
+    original_graph = graph.copy()
+    graph = apply_retiming(graph, retiming)
+
+    assert graph.number_of_nodes() == original_graph.number_of_nodes()
+    assert np.array_equal(graph.edges, original_graph.edges)
+    assert np.array_equal(graph.nodes, original_graph.nodes)
+
+    assert graph.edges[0, 1]['weight'] == 1.0
+    assert graph.edges[1, 2]['weight'] == 0.0
+    assert graph.edges[1, 3]['weight'] == 1.0
+    assert graph.edges[2, 3]['weight'] == 1.0
+    assert graph.edges[3, 0]['weight'] == 0.0
