32df08aa406d0b03203f38b61d0914ef4a943d43
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-13 10:15:28 +0200

    common docs

--- algorithms/common.py
+++ algorithms/common.py
@@ -7,6 +7,14 @@ __all__ = ['apply_retiming',
 
 
 def apply_retiming(graph: nx.DiGraph, r) -> nx.DiGraph:
+    """
+    Applies retiming passed as argument to input graph.
+
+    :param graph: A directed graph
+    :param r: A retiming dictionary
+    :return: Retimed graph
+    """
+
     retimed_graph = nx.DiGraph()
 
     retimed_graph.add_weighted_edges_from(
@@ -21,6 +29,13 @@ def apply_retiming(graph: nx.DiGraph, r) -> nx.DiGraph:
 
 
 def check_if_legal(graph: nx.DiGraph) -> bool:
+    """
+    Checks whether or not the graph is legal or not.
+
+    :param graph: A directed graph
+    :return: Boolean that tells if the graph is legal
+    """
+
     for edge in graph.edges:
         if graph.edges[edge]['weight'] < 0:
             return False
@@ -29,27 +44,42 @@ def check_if_legal(graph: nx.DiGraph) -> bool:
 
 
 def remove_backward_cycles(graph):
+    """
+    Removes every backward loop in directed graph.
+
+    i.e. In a graph [(0, 1), (1, 2), (2, 3), (3, 0), (2, 1)] the algorithm will remove (2, 1) and (3, 0)
+
+    :param graph: A directed graph
+    :return: A directed graph without backward loops
+    """
+
     max_node = max(graph.nodes)
     min_node = min(graph.nodes)
 
     try:
         graph.remove_edge(min_node, max_node)
     except nx.exception.NetworkXError:
-        print("")
+        pass
 
-    graph.remove_edges_from([(u, v) for (u, v) in graph.edges if u > v and (u, v) != (max_node, min_node)])
+    graph.remove_edges_from(
+        [(u, v) for (u, v) in graph.edges if u > v and (u, v) != (max_node, min_node)]
+    )
 
     return graph
 
 
-def get_subgraph_with_weight(graph: nx.DiGraph, weight, copy=True) -> nx.DiGraph:
-    weight_mask = graph.edges['weight'] == weight
+def get_subgraph_with_weight(graph: nx.DiGraph, weight: float) -> nx.DiGraph:
+    """
+    Returns a graph that have as edges edges of the graph passed as input with a specified weight passed as input
 
-    if copy:
-        copy_graph = nx.DiGraph()
-        copy_graph.add_weighted_edges_from(
-            [(u, v, 0.0) for (u, v) in graph.edges[weight_mask]]
-        )
-        return copy_graph
-    else:
-        raise NotImplemented
+    :param graph: A directed graph
+    :param weight: Weight that edges have to have to be included in the returned graph
+    :return: A directed graph created with edges taken from input graph and have the specified weight
+    """
+
+    copy_graph = nx.DiGraph()
+    copy_graph.add_weighted_edges_from(
+        [(edge, w) for edge, w in nx.get_edge_attributes(graph, 'weight').items() if w == weight]
+    )
+
+    return copy_graph
