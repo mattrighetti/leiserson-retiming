1ad72ca2962ae9266120bb2324eed72d844efb6a
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-13 11:12:45 +0200

    FEAS docs

--- algorithms/feas.py
+++ algorithms/feas.py
@@ -6,24 +6,28 @@ from algorithms.clock_period import delta_cp, cp
 __all__ = ['binary_search']
 
 
-def feas(graph_in: nx.DiGraph, clock_period) -> dict:
-    graph = graph_in.copy()
-    r_vertices = {}
+def feas(graph: nx.DiGraph, clock_period) -> dict:
+    """
+    Applies FEAS algorithm to input graph with a specified clock and returns a retiming dictionary to achieve it
 
-    for v in graph.nodes:
-        r_vertices[v] = 0
+    :param graph:
+    :param clock_period:
+    :return:
+    """
+    copy_graph = graph.copy()
+    r_vertices = dict([(v, 0.0) for v in copy_graph.nodes])
 
-    num_vertices = graph.number_of_nodes()
+    num_vertices = copy_graph.number_of_nodes()
 
     # 2. Repeat the following |V| - 1 times
     for _ in range(num_vertices - 1):
-        graph = apply_retiming(graph_in, r_vertices)
-        deltas = delta_cp(graph)
+        copy_graph = apply_retiming(graph, r_vertices)
+        deltas = delta_cp(copy_graph)
         for (key, value) in deltas.items():
             if value > clock_period:
                 r_vertices[key] = r_vertices[key] + 1
 
-    if cp(graph) > clock_period:
+    if cp(copy_graph) > clock_period:
         valid = False
     else:
         valid = True
@@ -33,7 +37,7 @@ def feas(graph_in: nx.DiGraph, clock_period) -> dict:
 
 def binary_search(graph: nx.DiGraph, D_sorted):
     """
-    Binary search feasible clock using Bellman-Ford algorithm
+    Binary search feasible clock using FEAS algorithm
 
     source: http://recipe.ee.ntu.edu.tw/LabWebsite/miniworkshop_Opt+App/Session%204-1%20JHJiang%20[相容模式].pdf
 
