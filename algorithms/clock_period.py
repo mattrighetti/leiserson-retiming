07dcaad33789f48f36cce7027a5524e05509dafa
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-13 09:37:43 +0200

    CP docs

--- algorithms/clock_period.py
+++ algorithms/clock_period.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import numpy as np
+import itertools
 
 __all__ = ['cp',
            'delta_cp',
@@ -6,11 +8,26 @@ __all__ = ['cp',
 
 
 def cp(graph: nx.DiGraph) -> float:
+    """
+    Returns the minimum clock period of a directed graph.
+
+    :param graph: A directed graph
+    :return: Minimum clock period of graph
+    """
+
     delta_vertices = all_delta_cp(graph)
     return max(delta_vertices.values())
 
 
 def delta_cp(graph: nx.DiGraph) -> dict:
+    """
+    Returns a dictionary that contains delta values needed for the CP algorithm.
+    The dictionary will only contain delta values for nodes that are on edges that have
+    s weight of 0.
+
+    :param graph: A directed graph
+    :return: Dictionary of delta values of nodes that have edges of weight 0
+    """
     zero_edges_subgraph = nx.DiGraph()
 
     zero_edges = [edge for edge in graph.edges if graph.edges[edge]['weight'] == 0]
@@ -20,18 +37,20 @@ def delta_cp(graph: nx.DiGraph) -> dict:
     delta_vertices = {}
 
     for v in zero_edges_subgraph_ordered:
-        max_incoming_u_delta = 0
-
-        for (u, _) in zero_edges_subgraph.in_edges(v):
-            if delta_vertices[u] > max_incoming_u_delta:
-                max_incoming_u_delta = delta_vertices[u]
-
-        delta_vertices[v] = max_incoming_u_delta + graph.nodes[v]['delay']
+        max_incoming_u_delta = [delta_vertices.get(u, 0) for (u, _) in zero_edges_subgraph.in_edges(v)]
+        delta_vertices[v] = max(max_incoming_u_delta, default=0) + graph.nodes[v]['delay']
 
     return delta_vertices
 
 
 def all_delta_cp(graph: nx.DiGraph) -> dict:
+    """
+    Returns a dictionary that contains delta values needed for the CP algorithm.
+    The dictionary will contain delta values for every node in graph passed as argument.
+
+    :param graph: A directed graph
+    :return: Dictionary of delta values of every node in graph
+    """
     zero_edges_subgraph = nx.DiGraph()
 
     zero_edges = [edge for edge in graph.edges if graph.edges[edge]['weight'] == 0]
@@ -41,13 +60,8 @@ def all_delta_cp(graph: nx.DiGraph) -> dict:
     delta_vertices = {}
 
     for v in zero_edges_subgraph_ordered:
-        max_incoming_u_delta = 0
-
-        for (u, _) in zero_edges_subgraph.in_edges(v):
-            if delta_vertices[u] > max_incoming_u_delta:
-                max_incoming_u_delta = delta_vertices[u]
-
-        delta_vertices[v] = max_incoming_u_delta + graph.nodes[v]['delay']
+        max_incoming_u_delta = [delta_vertices.get(u, 0) for (u, _) in zero_edges_subgraph.in_edges(v)]
+        delta_vertices[v] = max(max_incoming_u_delta, default=0) + graph.nodes[v]['delay']
 
     for non_zero_node in graph.nodes:
         if non_zero_node not in zero_edges_subgraph.nodes:
