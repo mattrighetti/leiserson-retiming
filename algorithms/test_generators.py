5cbf4bc0ec01dccb9cfff4e43155212484636fef
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-14 09:56:19 +0200

    test_generators docs

--- algorithms/test_generators.py
+++ algorithms/test_generators.py
@@ -8,6 +8,13 @@ __all__ = ['generate_all_weight_one_graph',
 
 
 def generate_all_weight_one_graph(num_nodes=10, p=0.25) -> nx.DiGraph:
+    """
+    Generates a graph that has edge weight equal to 1.0
+
+    :param num_nodes: Number of nodes that the graph must have
+    :param p: Probability used by the graph generator to decide how dense the graph will be
+    :return: Generated graph
+    """
     circle_graph = nx.path_graph(num_nodes, create_using=nx.DiGraph)
     circle_graph.add_edge(num_nodes - 1, 0, weight=1.0)
     random_graph = nx.binomial_graph(n=num_nodes, p=p, seed=27, directed=True)
@@ -25,6 +32,13 @@ def generate_all_weight_one_graph(num_nodes=10, p=0.25) -> nx.DiGraph:
 
 
 def check_legality(graph: nx.DiGraph) -> bool:
+    """
+    Checks if a graph satisfies condition W1 (all edges must have weight grater or equal than zero)
+    provided in the paper
+
+    :param graph: Graph on which to check condition W1
+    :return: True if legal, False if not legal
+    """
     for edge in graph.edges:
         if graph.edges[edge]['weight'] < 0:
             return False
@@ -33,6 +47,12 @@ def check_legality(graph: nx.DiGraph) -> bool:
 
 
 def get_random_retiming(graph: nx.DiGraph) -> dict:
+    """
+    Generates a random and legal retiming on a graph that has weight 1.0 on all of its edges
+
+    :param graph: nx.DiGraph with weight greater than zero on its edges
+    :return: Legal retiming dictionary
+    """
     r = dict([(node, 0) for node in graph.nodes])
 
     for node in graph.nodes:
