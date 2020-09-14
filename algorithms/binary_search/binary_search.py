955cf04761209949f043e12b0a384d9430b251f0
Mattia Righetti <matt95.righetti@gmail.com>
2020-09-14 09:59:14 +0200

    binary_search docs

--- algorithms/binary_search/binary_search.py
+++ algorithms/binary_search/binary_search.py
@@ -1,7 +1,18 @@
 import numpy as np
+from algorithms.retiming.strategies import *
 
 
 def binary_search(search_algorithm):
+    """
+    Binary search meant to be used as a decorator for algorithms OPT1 and OPT2
+
+    :param search_algorithm: OPT1 or OPT2 algorithms
+    :return:
+    """
+
+    if search_algorithm != opt1 and search_algorithm != opt2:
+        raise Exception("Cannot apply this specific binary search to specified search_algorithm")
+
     def bin_search(*args):
         low = 0
         high = len(args[1]) - 1
